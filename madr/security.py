3d34852194e7c1e866c3b75d9ec2e751646fba8c
chris <christyancosta@gmail.com>
2024-08-11 11:38:23 -0300

    Criando função get_current_user(), para autenticação de usuários nos endpoints

--- madr/security.py
+++ madr/security.py
@@ -1,17 +1,25 @@
 import html
 import re
 from datetime import datetime, timedelta
+from http import HTTPStatus
 
+from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
-from jwt import encode
+from jwt import ExpiredSignatureError, decode, encode
+from jwt.exceptions import PyJWKError
 from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 from zoneinfo import ZoneInfo
 
+from madr.database import get_session
+from madr.models import User
+from madr.schemas import TokenData
 from madr.settings import Settings
 
 pwd_context = PasswordHash.recommended()
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/token')
+settings = Settings()
 
 
 def get_password_hash(password: str):
@@ -46,12 +54,47 @@ def sanitize_string(input_string: str) -> str:
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(tz=ZoneInfo('UTC')) + timedelta(
-        minutes=Settings().ACCESS_TOKEN_EXPIRE_MINUTES
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
     to_encode.update({'exp': expire})
 
     encoded_jwt = encode(
-        to_encode, Settings().SECRET_KEY, algorithm=Settings().ALGORITHM
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
 
     return encoded_jwt
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: Session = Depends(get_session),
+):
+    credentials_exception = HTTPException(
+        status_code=HTTPStatus.UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    try:
+        payload = decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+
+        username = payload.get('sub')
+        if not username:
+            raise credentials_exception
+
+        token_data = TokenData(username=username)
+    except ExpiredSignatureError:
+        raise credentials_exception
+    except PyJWKError:
+        raise credentials_exception
+
+    user = session.scalar(
+        select(User).where(User.email == token_data.username)
+    )
+
+    if not user:
+        raise credentials_exception
+
+    return user
