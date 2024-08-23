58d7b86c801df6246f0f2a0e4938205528a0650d
chris <christyancosta@gmail.com>
2024-08-22 23:34:47 -0300

    Adicionando test_get_filtro_de_livros

--- tests/test_livro.py
+++ tests/test_livro.py
@@ -40,3 +40,64 @@ def test_get_busca_livro_por_id(client, book_2):
     assert response.json()['year'] == 1974  # noqa
     assert response.json()['title'] == 'café da manhã dos campeões'
     assert response.json()['romancista_id'] == 1
+
+
+def test_delete_livro(client, book_1, token):
+    response = client.delete(
+        '/livro/1', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Livro deletado do MADR'}
+
+
+def test_get_filtro_de_livros(client, token):
+    client.post(
+        '/livro',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'year': 1900,
+            'title': 'Café Da Manhã Dos Campeões',
+            'romancista_id': 1,
+        },
+    )
+
+    client.post(
+        '/livro',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'year': 1900,
+            'title': 'Memórias Póstumas de Brás Cubas',
+            'romancista_id': 2,
+        },
+    )
+
+    client.post(
+        '/livro',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'year': 1865,
+            'title': 'Iracema',
+            'romancista_id': 3,
+        },
+    )
+
+    response = client.get('/livro/?title=a&year=1900')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'livros': [
+            {
+                'year': 1900,
+                'title': 'café da manhã dos campeões',
+                'romancista_id': 1,
+                'id': 1,
+            },
+            {
+                'year': 1900,
+                'title': 'memórias póstumas de brás cubas',
+                'romancista_id': 2,
+                'id': 2,
+            },
+        ]
+    }
