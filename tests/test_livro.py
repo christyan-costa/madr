fca0fcab4f7548ce4c21bfdb7d1d7a103b117f7e
chris <christyancosta@gmail.com>
2024-08-22 23:19:43 -0300

    Adicionando teste para endpoint GET /livro/{id}

--- tests/test_livro.py
+++ tests/test_livro.py
@@ -18,17 +18,7 @@ def test_post_livro_deve_receber_status_201_created(client, token):
     assert response.json()['romancista_id'] == 1
 
 
-def test_quando_enviar_patch_deve_receber_status_200(client, token):
-    client.post(
-        '/livro',
-        headers={'Authorization': f'Bearer {token}'},
-        json={
-            'year': 1973,
-            'title': 'Café Da Manhã Dos Campeões',
-            'romancista_id': 1,
-        },
-    )
-
+def test_patch_alteracao_de_livro(client, token, book_1):
     response = client.patch(
         '/livro/1',
         headers={'Authorization': f'Bearer {token}'},
@@ -36,6 +26,15 @@ def test_quando_enviar_patch_deve_receber_status_200(client, token):
             'year': 1974,
         },
     )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json()['year'] == 1974  # noqa
+    assert response.json()['title'] == 'café da manhã dos campeões'
+    assert response.json()['romancista_id'] == 1
+
+
+def test_get_busca_livro_por_id(client, book_2):
+    response = client.get('livro/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json()['year'] == 1974  # noqa
