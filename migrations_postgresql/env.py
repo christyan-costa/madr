29cfdbd5355a75014d1a0f8363f906432a63eb43
chris <christyancosta@gmail.com>
2024-08-24 17:17:34 -0300

    Adicionando migrações para o PostgreSQL

--- migrations_postgresql/env.py
+++ migrations_postgresql/env.py
@@ -1,13 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from madr.models import table_registry
 from madr.settings import Settings
-
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -43,12 +40,12 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option('sqlalchemy.url')
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
+        dialect_opts={'paramstyle': 'named'},
     )
 
     with context.begin_transaction():
@@ -64,7 +61,7 @@ def run_migrations_online() -> None:
     """
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+        prefix='sqlalchemy.',
         poolclass=pool.NullPool,
     )
 
